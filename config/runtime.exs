80767bfa57b79e01b2614eccfed1911ddc101a97
gmorell <gmp@gmp.io>
2022-08-05 18:34:35 -0400

    remove types

--- config/runtime.exs
+++ config/runtime.exs
@@ -83,8 +83,7 @@ if config_env() == :prod do
          # ssl: true,
          url: database_url,
          pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-         socket_options: maybe_ipv6,
-         types: RoomHermes.PostgresTypes
+         socket_options: maybe_ipv6
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
