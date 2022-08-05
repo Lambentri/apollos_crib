1e0f31b1e58afbba93042a644e8b5af8fbe17f13
gmorell <gmp@gmp.io>
2022-08-05 18:45:52 -0400

    whoops

--- config/runtime.exs
+++ config/runtime.exs
@@ -72,14 +72,16 @@ if config_env() == :prod do
 
   maybe_ipv6 = if System.get_env("ECTO_IPV6"), do: [:inet6], else: []
 
-  config :room_sanctum, RoomSanctum.Repo,
-    # ssl: true,
-    url: database_url,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    socket_options: maybe_ipv6,
-    types: RoomSanctum.PostgresTypes
+  config :room_sanctum,
+         RoomSanctum.Repo,
+         # ssl: true,
+         url: database_url,
+         pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+         socket_options: maybe_ipv6,
+         types: RoomSanctum.PostgresTypes
 
-  config :room_hermes, RoomHermes.Repo,
+  config :room_hermes,
+         RoomHermes.Repo,
          # ssl: true,
          url: database_url,
          pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
@@ -98,19 +100,40 @@ if config_env() == :prod do
       """
 
   host = System.get_env("PHX_HOST") || "example.com"
-  port = String.to_integer(System.get_env("SPORT") || "4001")
+  sport = String.to_integer(System.get_env("SPORT") || "4000")
+  hport = String.to_integer(System.get_env("HPORT") || "4000")
 
-  config :room_sanctum, RoomSanctumWeb.Endpoint,
-    url: [host: host, port: 443],
-    http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
-    ],
-    secret_key_base: secret_key_base
+  config :room_sanctum,
+         RoomSanctumWeb.Endpoint,
+         url: [
+           host: host,
+           port: 443
+         ],
+         http: [
+           # Enable IPv6 and bind on all interfaces.
+           # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
+           # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
+           # for details about using IPv6 vs IPv4 and loopback vs public addresses.
+           ip: {0, 0, 0, 0, 0, 0, 0, 0},
+           port: sport
+         ],
+         secret_key_base: secret_key_base
+
+  config :room_hermes,
+         RoomHermesWeb.Endpoint,
+         url: [
+           host: host,
+           port: 443
+         ],
+         http: [
+           # Enable IPv6 and bind on all interfaces.
+           # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
+           # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
+           # for details about using IPv6 vs IPv4 and loopback vs public addresses.
+           ip: {0, 0, 0, 0, 0, 0, 0, 0},
+           port: hport
+         ],
+         secret_key_base: secret_key_base
 
   # ## Using releases
   #
