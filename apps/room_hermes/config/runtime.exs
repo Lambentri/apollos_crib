060a00c329cc0b2d674f2f224fb18e3cf06bc4ff
gmorell <gmp@gmp.io>
2022-08-20 00:39:06 -0400

    moving over.

--- apps/room_hermes/config/runtime.exs
+++ apps/room_hermes/config/runtime.exs
@@ -42,13 +42,13 @@ if config_env() == :prod do
       """
 
   host = System.get_env("PHX_HOST") || "example.com"
-  port = String.to_integer(System.get_env("PORT") || "4001")
+  port = String.to_integer(System.get_env("HPORT") || "4001")
 
-  config :room_sanctum,
+  config :room_hermes,
          RoomHermesWeb.Endpoint,
          url: [
            host: host,
-           port: 443
+           port: 444
          ],
          http: [
            # Enable IPv6 and bind on all interfaces.
