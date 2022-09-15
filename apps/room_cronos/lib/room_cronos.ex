d4f77d002c436925e40ed8739520623934d621f0
gmorell <gmp@gmp.io>
2022-09-15 13:10:56 -0400

    missing cronos from git ffff

---
+++ apps/room_cronos/lib/room_cronos.ex
@@ -0,0 +1,18 @@
+defmodule RoomCronos do
+  @moduledoc """
+  Documentation for `RoomCronos`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RoomCronos.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/room_cronos/test/room_cronos_test.exs
@@ -0,0 +1,8 @@
+defmodule RoomCronosTest do
+  use ExUnit.Case
+  doctest RoomCronos
+
+  test "greets the world" do
+    assert RoomCronos.hello() == :world
+  end
+end
