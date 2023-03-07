d3426ed2832780fe47d53bdcaf7f7123a81985e4
gmorell <gmp@gmp.io>
2023-03-07 00:38:07 -0500

    remove sentry from hermes?

--- apps/room_hermes/lib/room_hermes_web/endpoint.ex
+++ apps/room_hermes/lib/room_hermes_web/endpoint.ex
@@ -1,6 +1,5 @@
 defmodule RoomHermesWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :room_hermes
-  use Sentry.PlugCapture
 
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
