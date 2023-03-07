3411654f2dc06832cd443b6431d3ed3cb08915c8
gmorell <gmp@gmp.io>
2023-03-07 01:16:49 -0500

    add short-circuit to allow when the env auth is passed back

--- apps/room_hermes/lib/room_hermes_web/controllers/user_controller.ex
+++ apps/room_hermes/lib/room_hermes_web/controllers/user_controller.ex
@@ -6,14 +6,21 @@ defmodule RoomHermesWeb.UserController do
 
   action_fallback RoomHermesWeb.FallbackController
 
-  def create(conn, %{"username" => username, "password" => password, "client_id" => client_id}) do
-    case client_id == username do
-      true ->
-        case Accounts.find_rabbit_user(username, password) do
-          nil -> send_resp(conn, :ok, "deny")
-          val -> send_resp(conn, :ok, "allow")
+  def create(conn, %{"username" => username, "password" => password} = args) do
+    # rabbit decided to misbehave and pass even the local auth thru rabbitauth so we're doing a short circuit
+    envauth = Application.get_env(:room_hermes, :rabbit)
+    cond do
+        username == envauth[:username] && password == envauth[:password] -> send_resp(conn, :ok, "allow")
+        true ->
+            case args["client_id"] == username do
+              true ->
+                case Accounts.find_rabbit_user(username, password) do
+                  nil -> send_resp(conn, :ok, "deny")
+                  val -> send_resp(conn, :ok, "allow")
+                end
+              false -> send_resp(conn, :ok, "deny")
+            end
         end
-      false -> send_resp(conn, :ok, "deny")
-    end
+
   end
 end
