c354696473fb34e1bd6770793a56947af3c17a04
gmorell <gmp@gmp.io>
2025-12-28 10:16:20 -0500

    ...

--- apps/room_sanctum/assets/js/app.js
+++ apps/room_sanctum/assets/js/app.js
@@ -373,7 +373,7 @@ Hooks.mkShowMap = {
     }
 }
 
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
+// let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {
   params: {_csrf_token: csrfToken},
   hooks: {...Hooks, LeafletMap, LeafletMapBridge}
