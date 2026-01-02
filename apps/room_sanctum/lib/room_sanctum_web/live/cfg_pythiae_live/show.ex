f8d2b7e31ba82c18304681befaef4dfa979bd2b9
gmorell <gmp@gmp.io>
2026-01-01 21:20:50 -0500

    use the old one on the preview

--- apps/room_sanctum/lib/room_sanctum_web/live/cfg_pythiae_live/show.ex
+++ apps/room_sanctum/lib/room_sanctum_web/live/cfg_pythiae_live/show.ex
@@ -59,7 +59,7 @@ defmodule RoomSanctumWeb.PythiaeLive.Show do
   defp page_title(:edit), do: "Edit Pythiae"
 
   defp condense({id, type}, data) do
-    RoomSanctum.Condenser.BasicMQTT.condense({id, type}, data)
+    RoomSanctum.Condenser.BasicMQTT.condense_data({id, type}, data)
   end
 
   defp get_icon(type) do
